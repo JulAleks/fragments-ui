e652d3809b88af19b82b6f3990a8b58740b92c22
Julia Alekseev <yael.julia@gmail.com>
2024-09-12 16:44:11 -0400

    added aws and auth

--- src/app.js
+++ src/app.js
@@ -1,2 +1,45 @@
-// JS that will appear in the browser console (Shift + CTRL + J)
-console.log('Parcel is working!');
+// src/app.js
+// console logs will show in browser (Shift + CTRL + J)
+import { Auth, getUser } from './auth';
+
+async function init() {
+  // Get our UI elements
+  const userSection = document.querySelector('#user');
+  const loginBtn = document.querySelector('#login');
+  const logoutBtn = document.querySelector('#logout');
+
+  // Wire up event handlers to deal with login and logout.
+  loginBtn.onclick = () => {
+    // Sign-in via the Amazon Cognito Hosted UI (requires redirects), see:
+    // https://docs.amplify.aws/lib/auth/advanced/q/platform/js/#identity-pool-federation
+    Auth.federatedSignIn();
+  };
+  logoutBtn.onclick = () => {
+    // Sign-out of the Amazon Cognito Hosted UI (requires redirects), see:
+    // https://docs.amplify.aws/lib/auth/emailpassword/q/platform/js/#sign-out
+    Auth.signOut();
+  };
+
+  // See if we're signed in (i.e., we'll have a `user` object)
+  const user = await getUser();
+  if (!user) {
+    // Disable the Logout button
+    logoutBtn.disabled = true;
+    return;
+  }
+
+  // Log the user info for debugging purposes
+  console.log({ user });
+
+  // Update the UI to welcome the user
+  userSection.hidden = false;
+
+  // Show the user's username
+  userSection.querySelector('.username').innerText = user.username;
+
+  // Disable the Login button
+  loginBtn.disabled = true;
+}
+
+// Wait for the DOM to be ready, then start the app
+addEventListener('DOMContentLoaded', init);
