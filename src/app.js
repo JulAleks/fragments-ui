60c86b39b488fbf458fc8344b359a96f4f60dd38
Julia Alekseev <yael.julia@gmail.com>
2024-09-26 16:25:08 -0400

    Added logs

--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@
 // console logs will show in browser (Shift + CTRL + J)
 import { Auth, getUser } from './auth';
 import { getUserFragments } from './api';
+const logger = require('./logger');
 
 async function init() {
   // Get our UI elements
@@ -18,31 +19,38 @@ async function init() {
   logoutBtn.onclick = () => {
     // Sign-out of the Amazon Cognito Hosted UI (requires redirects), see:
     // https://docs.amplify.aws/lib/auth/emailpassword/q/platform/js/#sign-out
+    logger.info('Login button clicked');
     Auth.signOut();
   };
 
   // See if we're signed in (i.e., we'll have a `user` object)
-  const user = await getUser();
-  if (!user) {
-    // Disable the Logout button
-    logoutBtn.disabled = true;
-    return;
+  try {
+    const user = await getUser();
+    if (!user) {
+      // Disable the Logout button
+      logoutBtn.disabled = true;
+      logger.info('Logout button clicked');
+      return;
+    }
+
+    // Do an authenticated request to the fragments API server and log the result
+    const userFragments = await getUserFragments(user);
+    logger.info({ userFragments }, 'Fetched user fragments');
+
+    // Update the UI to welcome the user
+    userSection.hidden = false;
+
+    // Show the user's username
+    userSection.querySelector('.username').innerText = user.username;
+
+    // Disable the Login button
+    loginBtn.disabled = true;
+
+    // Logging that user is signed in
+    logger.info({ user }, 'User is signed in');
+  } catch (error) {
+    logger.error(error, 'Error during initialization');
   }
-
-  // Do an authenticated request to the fragments API server and log the result
-  const userFragments = await getUserFragments(user);
-
-  // Log the user info for debugging purposes
-  console.log({ user });
-
-  // Update the UI to welcome the user
-  userSection.hidden = false;
-
-  // Show the user's username
-  userSection.querySelector('.username').innerText = user.username;
-
-  // Disable the Login button
-  loginBtn.disabled = true;
 }
 
 // Wait for the DOM to be ready, then start the app
